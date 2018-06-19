66eef6dc9c92c534e4a2cfb41c562aa192dcd31d
Mateusz Bednarski <echelon133@gmail.com>
2018-06-19 12:30:37 +0200

    Added main application class - SpringLibraryApp

---
+++ src/main/java/ml/echelon133/SpringLibraryApp.java
@@ -0,0 +1,12 @@
+package ml.echelon133;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLibraryApp.class, args);
+    }
+}
