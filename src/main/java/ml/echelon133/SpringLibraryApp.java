d7c5522e3d4396e7091bc17388888f7c567c25d6
Mateusz Bednarski <echelon133@gmail.com>
2018-06-20 12:05:01 +0200

    Request scoped ErrorMessage bean is now available in webapp context

--- src/main/java/ml/echelon133/SpringLibraryApp.java
+++ src/main/java/ml/echelon133/SpringLibraryApp.java
@@ -1,10 +1,21 @@
 package ml.echelon133;
 
+import ml.echelon133.model.message.ErrorMessage;
+import ml.echelon133.model.message.IErrorMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootApplication
 public class SpringLibraryApp {
+
+    @Bean
+    @Scope(scopeName = WebApplicationContext.SCOPE_REQUEST)
+    public IErrorMessage errorMessage() {
+        return new ErrorMessage();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringLibraryApp.class, args);
