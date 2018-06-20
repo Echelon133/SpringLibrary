f1f16966edef944cc93e69f77cb2e91cd161fdab
Mateusz Bednarski <echelon133@gmail.com>
2018-06-20 10:01:11 +0200

    Updated GenreController - now it uses IGenreService

--- src/main/java/ml/echelon133/controller/GenreController.java
+++ src/main/java/ml/echelon133/controller/GenreController.java
@@ -1,21 +1,27 @@
 package ml.echelon133.controller;
 
 import ml.echelon133.model.Genre;
+import ml.echelon133.service.IGenreService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class GenreController {
 
+    private IGenreService genreService;
+
+    public GenreController(IGenreService genreService) {
+        this.genreService = genreService;
+    }
+
     @RequestMapping(value = "api/genres", method = RequestMethod.GET)
     public ResponseEntity<List<Genre>> getGenres() {
-        List<Genre> genres = new ArrayList<>();
+        List<Genre> genres = genreService.findAll();
         return new ResponseEntity<>(genres, HttpStatus.OK);
     }
 }
