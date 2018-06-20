905588ae80a8b1a209c24fcc25516da9d9457f1c
Mateusz Bednarski <echelon133@gmail.com>
2018-06-20 12:01:45 +0200

    Added ErrorMessage interface and implementation

---
+++ src/main/java/ml/echelon133/model/message/IErrorMessage.java
@@ -0,0 +1,14 @@
+package ml.echelon133.model.message;
+
+import java.util.Date;
+import java.util.List;
+
+public interface IErrorMessage {
+    Date getTimestamp();
+    List<String> getMessages();
+    String getPath();
+    void setTimestamp(Date timestamp);
+    void setMessages(List<String> messages);
+    void setPath(String path);
+    void addSingleMessage(String message);
+}
