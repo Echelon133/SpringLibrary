1d14ead52132ae7fa33a2c22879d2fb3d01d1cae
Mateusz Bednarski <echelon133@gmail.com>
2018-06-20 12:08:40 +0200

    Updated GenreController with GET api/genres/{id} endpoint

--- src/main/java/ml/echelon133/controller/GenreController.java
+++ src/main/java/ml/echelon133/controller/GenreController.java
@@ -1,9 +1,11 @@
 package ml.echelon133.controller;
 
+import ml.echelon133.exception.ResourceNotFoundException;
 import ml.echelon133.model.Genre;
 import ml.echelon133.service.IGenreService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +25,11 @@ public class GenreController {
     public ResponseEntity<List<Genre>> getGenres() {
         List<Genre> genres = genreService.findAll();
         return new ResponseEntity<>(genres, HttpStatus.OK);
+    }
+
+    @RequestMapping(value= "api/genres/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Genre> getGenre(@PathVariable Long id) throws ResourceNotFoundException {
+        Genre genre = genreService.findById(id);
+        return new ResponseEntity<>(genre, HttpStatus.OK);
     }
 }
