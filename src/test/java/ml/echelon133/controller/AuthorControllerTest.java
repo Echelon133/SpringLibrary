80707c1b4dacfb13d8eb4e891832b2bb983afd84
Mateusz Bednarski <echelon133@gmail.com>
2018-07-07 15:18:53 +0200

    Updated AuthorControllerTest with more tests of GET api/authors endpoint

--- src/test/java/ml/echelon133/controller/AuthorControllerTest.java
+++ src/test/java/ml/echelon133/controller/AuthorControllerTest.java
@@ -12,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -20,6 +21,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
@@ -67,6 +69,53 @@ public class AuthorControllerTest {
         // Then
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.getContentAsString()).isEqualTo("[]");
+    }
+
+    @Test
+    public void getNotEmptyAuthorsList() throws Exception {
+        Author author1 = new Author("First Author", "First Author description");
+        Author author2 = new Author("Second Author", "Second Author description");
+        List<Author> authors = Arrays.asList(author1, author2);
+
+        // Expected Json
+        JsonContent<List<Author>> jsonAuthorContent = jsonAuthors.write(authors);
+
+        // Given
+        given(authorService.findAll()).willReturn(authors);
+
+        // When
+        MockHttpServletResponse response = mvc.perform(
+                get("/api/authors")
+                        .accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        // Then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).isEqualTo(jsonAuthorContent.getJson());
+    }
+
+    @Test
+    public void getFilteredAuthorsList() throws Exception {
+        Author author1 = new Author("John First", "John First description");
+        author1.setId(1L);
+        Author author2 = new Author("John Third", "John Third description");
+        author2.setId(2L);
+        List<Author> expectedAuthors = Arrays.asList(author1, author2);
+
+        // Expected json
+        JsonContent<List<Author>> jsonAuthorContent = jsonAuthors.write(expectedAuthors);
+
+        // Given
+        given(authorService.findAllByNameContaining("John")).willReturn(expectedAuthors);
+
+        // When
+        MockHttpServletResponse response = mvc.perform(
+                get("/api/authors")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .param("name", "John")).andReturn().getResponse();
+
+        // Then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).isEqualTo(jsonAuthorContent.getJson());
     }
 
 }
