6de138c037a60332e07e57012df102b1fba2fa06
Mateusz Bednarski <echelon133@gmail.com>
2018-07-08 15:41:40 +0200

    Updated AuthorController with implementation of POST api/authors endpoint

--- src/main/java/ml/echelon133/controller/AuthorController.java
+++ src/main/java/ml/echelon133/controller/AuthorController.java
@@ -1,15 +1,16 @@
 package ml.echelon133.controller;
 
+import ml.echelon133.exception.FailedFieldValidationException;
 import ml.echelon133.model.Author;
+import ml.echelon133.model.dto.AuthorDto;
 import ml.echelon133.service.IAuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -31,5 +32,15 @@ public class AuthorController {
             authors = authorService.findAllByNameContaining(name);
         }
         return new ResponseEntity<>(authors, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "api/authors", method = RequestMethod.POST)
+    public ResponseEntity<Author> postAuthor(@Valid @RequestBody AuthorDto authorDto, BindingResult result) throws FailedFieldValidationException {
+        if (result.hasErrors()) {
+            throw new FailedFieldValidationException(result.getFieldErrors());
+        }
+        Author author = new Author(authorDto.getName(), authorDto.getDescription());
+        Author savedAuthor = authorService.save(author);
+        return new ResponseEntity<>(savedAuthor, HttpStatus.CREATED);
     }
 }
