1a046b178e2ce9c46a3a45b264e9f0f4a8f4f1ff
Mateusz Bednarski <echelon133@gmail.com>
2018-07-10 18:54:22 +0200

    Added Book entity class

---
+++ src/main/java/ml/echelon133/model/Book.java
@@ -0,0 +1,82 @@
+package ml.echelon133.model;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(
+            name = "book_authors",
+            joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id", referencedColumnName = "id")
+    )
+    private Set<Author> authors;
+
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(
+            name = "book_genres",
+            joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id", referencedColumnName = "id")
+    )
+    private List<Genre> genres;
+
+    @OneToOne(mappedBy="bookInfoOwner", cascade=CascadeType.PERSIST)
+    private BookInfo bookInfo;
+
+    public Book() {}
+    public Book(String title, Set<Author> authors, List<Genre> genres) {
+        this.title = title;
+        this.authors = authors;
+        this.genres = genres;
+        setBookInfo(new BookInfo());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
+    }
+
+    public BookInfo getBookInfo() {
+        return bookInfo;
+    }
+
+    public void setBookInfo(BookInfo bookInfo) {
+        bookInfo.setBookInfoOwner(this);
+        this.bookInfo = bookInfo;
+    }
+}
