5dc52c2ba2aca22fb1d00e1dc242a538fbaf3fae
Mateusz Bednarski <echelon133@gmail.com>
2018-07-12 18:27:39 +0200

    Added BookController with initial implementation of GET api/books endpoint

---
+++ src/main/java/ml/echelon133/controller/BookController.java
@@ -0,0 +1,32 @@
+package ml.echelon133.controller;
+
+import ml.echelon133.model.Book;
+import ml.echelon133.service.IBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    private IBookService bookService;
+
+    @Autowired
+    public BookController(IBookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping(value = "api/books", method = RequestMethod.GET)
+    public ResponseEntity<List<Book>> getBooks(@RequestParam(value = "title", required = false) String title,
+                                               @RequestParam(value = "author", required = false) String author,
+                                               @RequestParam(value = "genre", required = false) String genre) {
+        List<Book> books = bookService.findAll();
+        return new ResponseEntity<>(books , HttpStatus.OK);
+    }
+}
