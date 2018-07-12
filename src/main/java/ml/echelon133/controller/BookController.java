105e559c5fb1a43bf29b7099989674c611b177fd
Mateusz Bednarski <echelon133@gmail.com>
2018-07-12 20:52:55 +0200

    Updated BookController with GET api/books/{id} endpoint implementation

--- src/main/java/ml/echelon133/controller/BookController.java
+++ src/main/java/ml/echelon133/controller/BookController.java
@@ -1,14 +1,12 @@
 package ml.echelon133.controller;
 
+import ml.echelon133.exception.ResourceNotFoundException;
 import ml.echelon133.model.Book;
 import ml.echelon133.service.IBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,5 +35,11 @@ public class BookController {
             books = bookService.findAll();
         }
         return new ResponseEntity<>(books , HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "api/books/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Book> getBook(@PathVariable Long id) throws ResourceNotFoundException {
+        Book book = bookService.findById(id);
+        return new ResponseEntity<>(book, HttpStatus.OK);
     }
 }
