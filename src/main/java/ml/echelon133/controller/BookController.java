d327e226a590be214355d6eb387c383708b17d97
Mateusz Bednarski <echelon133@gmail.com>
2018-07-16 16:17:18 +0200

    Updated BookController - added PATCH api/genre/{id} endpoint implementation

--- src/main/java/ml/echelon133/controller/BookController.java
+++ src/main/java/ml/echelon133/controller/BookController.java
@@ -6,6 +6,7 @@ import ml.echelon133.model.Author;
 import ml.echelon133.model.Book;
 import ml.echelon133.model.Genre;
 import ml.echelon133.model.dto.NewBookDto;
+import ml.echelon133.model.dto.PatchBookDto;
 import ml.echelon133.service.IAuthorService;
 import ml.echelon133.service.IBookService;
 import ml.echelon133.service.IGenreService;
@@ -52,12 +53,6 @@ public class BookController {
         return new ResponseEntity<>(books , HttpStatus.OK);
     }
 
-    @RequestMapping(value = "api/books/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Book> getBook(@PathVariable Long id) throws ResourceNotFoundException {
-        Book book = bookService.findById(id);
-        return new ResponseEntity<>(book, HttpStatus.OK);
-    }
-
     @RequestMapping(value = "api/books", method = RequestMethod.POST)
     public ResponseEntity<Book> postBook(@Valid @RequestBody NewBookDto newBookDto, BindingResult result)
             throws FailedFieldValidationException, ResourceNotFoundException {
@@ -78,6 +73,49 @@ public class BookController {
         Book book = new Book(newBookDto.getTitle(), bookAuthors, bookGenres);
         Book savedBook = bookService.save(book);
         return new ResponseEntity<>(savedBook, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "api/books/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Book> getBook(@PathVariable Long id) throws ResourceNotFoundException {
+        Book book = bookService.findById(id);
+        return new ResponseEntity<>(book, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "api/books/{id}", method = RequestMethod.PATCH)
+    public ResponseEntity<Book> patchBook(@PathVariable Long id, @Valid @RequestBody PatchBookDto patchBookDto, BindingResult result)
+        throws FailedFieldValidationException, ResourceNotFoundException {
+        if (result.hasErrors()) {
+            throw new FailedFieldValidationException(result.getFieldErrors());
+        }
+
+        String newTitle = patchBookDto.getTitle();
+        List<Long> newAuthorIds = patchBookDto.getAuthorIds();
+        List<Long> newGenreIds = patchBookDto.getGenreIds();
+
+        Book book = bookService.findById(id);
+
+        if (newTitle != null) {
+            book.setTitle(newTitle);
+        }
+
+        if (newAuthorIds != null) {
+            Set<Author> newAuthors = new HashSet<>();
+            for (Long authorId : newAuthorIds) {
+                newAuthors.add(authorService.findById(authorId));
+            }
+            book.setAuthors(newAuthors);
+        }
+
+        if (newGenreIds != null) {
+            List<Genre> newGenres = new ArrayList<>();
+            for (Long genreId : newGenreIds) {
+                newGenres.add(genreService.findById(genreId));
+            }
+            book.setGenres(newGenres);
+        }
+
+        Book patchedBook = bookService.save(book);
+        return new ResponseEntity<>(patchedBook, HttpStatus.OK);
     }
 
 }
