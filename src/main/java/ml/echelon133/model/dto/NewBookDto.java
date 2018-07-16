36c80e0c368ae71a3e017643f6eaa8955cf06cbd
Mateusz Bednarski <echelon133@gmail.com>
2018-07-16 14:49:08 +0200

    Renamed NewBookDto class

--- src/main/java/ml/echelon133/model/dto/NewBookDto.java
+++ src/main/java/ml/echelon133/model/dto/NewBookDto.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
 
-public class BookDto {
+public class NewBookDto {
 
     @NotNull
     @Length(min=1, max=255)
