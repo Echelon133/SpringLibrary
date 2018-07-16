3f60272622439bf3cc4dda7d04ea55ea98f6f939
Mateusz Bednarski <echelon133@gmail.com>
2018-07-16 15:25:55 +0200

    Added PatchBookDto

---
+++ src/main/java/ml/echelon133/model/dto/PatchBookDto.java
@@ -0,0 +1,41 @@
+package ml.echelon133.model.dto;
+
+import org.hibernate.validator.constraints.Length;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+public class PatchBookDto {
+
+    @Length(min=1, max=255)
+    private String title;
+
+    @Size(min = 1, max = 10)
+    private List<Long> authorIds;
+
+    @Size(min = 1, max = 10)
+    private List<Long> genreIds;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Long> getAuthorIds() {
+        return authorIds;
+    }
+
+    public void setAuthorIds(List<Long> authorIds) {
+        this.authorIds = authorIds;
+    }
+
+    public List<Long> getGenreIds() {
+        return genreIds;
+    }
+
+    public void setGenreIds(List<Long> genreIds) {
+        this.genreIds = genreIds;
+    }
+}
