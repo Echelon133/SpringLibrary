a233c9d7fe5a8963a14027dbe07f4d6bdf3894c5
Mateusz Bednarski <echelon133@gmail.com>
2018-07-18 18:04:12 +0200

    Added PatchBookInfoDto class

---
+++ src/main/java/ml/echelon133/model/dto/PatchBookInfoDto.java
@@ -0,0 +1,67 @@
+package ml.echelon133.model.dto;
+
+import org.hibernate.validator.constraints.ISBN;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Positive;
+
+public class PatchBookInfoDto {
+
+    @Digits(integer = 4, fraction = 0)
+    @Positive
+    private Integer numberOfPages;
+
+    @Length(min = 3, max = 30)
+    private String language;
+
+    @Digits(integer = 4, fraction = 0)
+    @Positive
+    private Integer publicationYear;
+
+    @Length(min = 10, max = 500)
+    private String description;
+
+    @ISBN
+    private String isbn;
+
+    public Integer getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(Integer numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Integer getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(Integer publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
