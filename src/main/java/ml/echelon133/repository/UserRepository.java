71459850aa0f197f6a1af6ff8769cfeb8309ddbf
Mateusz Bednarski <echelon133@gmail.com>
2018-07-24 19:25:08 +0200

    Added UserRepository

---
+++ src/main/java/ml/echelon133/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package ml.echelon133.repository;
+
+import ml.echelon133.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User getUserByUsername(String username);
+}
