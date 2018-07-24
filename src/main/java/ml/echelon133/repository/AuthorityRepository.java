54d94ea53ae3b994c9e11b26b0227cb390d67a7d
Mateusz Bednarski <echelon133@gmail.com>
2018-07-24 19:53:38 +0200

    Changed getByAuthority to findByAuthority in AuthorityRepository

--- src/main/java/ml/echelon133/repository/AuthorityRepository.java
+++ src/main/java/ml/echelon133/repository/AuthorityRepository.java
@@ -4,5 +4,5 @@ import ml.echelon133.model.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-    Authority getAuthorityByAuthority(String authorityName);
+    Authority findAuthorityByAuthority(String authorityName);
 }
