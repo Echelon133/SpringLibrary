5dffeae75deaf941cc3c03fe6fdd40dfd698d803
Mateusz Bednarski <echelon133@gmail.com>
2018-07-24 19:13:04 +0200

    Added Authority (implementing GrantedAuthority) entity class

---
+++ src/main/java/ml/echelon133/model/Authority.java
@@ -0,0 +1,39 @@
+package ml.echelon133.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Authority implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Authority() {}
+    public Authority(String authority) {
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
