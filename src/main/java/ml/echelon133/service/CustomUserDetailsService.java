edfc3bd3cf6e8c352fcd6f9e1a89fafde805bed9
Mateusz Bednarski <echelon133@gmail.com>
2018-07-25 22:24:18 +0200

    Added CustomUserDetailsService implementation

---
+++ src/main/java/ml/echelon133/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package ml.echelon133.service;
+
+import ml.echelon133.exception.ResourceNotFoundException;
+import ml.echelon133.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private IUserService userService;
+
+    @Autowired
+    public CustomUserDetailsService(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user;
+        try {
+            user = userService.findUserByUsername(username);
+        } catch (ResourceNotFoundException ex) {
+            throw new UsernameNotFoundException("Username not found");
+        }
+        return user;
+    }
+}
