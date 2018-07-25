6bdc3f5a8148b2cab1677c80b2172460272bd628
Mateusz Bednarski <echelon133@gmail.com>
2018-07-25 20:23:53 +0200

    Now implementation of ISecretGenerator is available as a bean

--- src/main/java/ml/echelon133/SpringLibraryApp.java
+++ src/main/java/ml/echelon133/SpringLibraryApp.java
@@ -2,6 +2,8 @@ package ml.echelon133;
 
 import ml.echelon133.model.message.ErrorMessage;
 import ml.echelon133.model.message.IErrorMessage;
+import ml.echelon133.security.secret.ISecretGenerator;
+import ml.echelon133.security.secret.SecretGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +17,11 @@ public class SpringLibraryApp {
     @Scope(scopeName = WebApplicationContext.SCOPE_REQUEST)
     public IErrorMessage errorMessage() {
         return new ErrorMessage();
+    }
+
+    @Bean
+    public ISecretGenerator secretGenerator() {
+        return new SecretGenerator(16);
     }
 
     public static void main(String[] args) {
