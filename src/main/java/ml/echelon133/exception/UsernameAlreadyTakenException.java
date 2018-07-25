454e91a9a491d5cb227dabf3d32136b3f0b41702
Mateusz Bednarski <echelon133@gmail.com>
2018-07-25 22:34:17 +0200

    Added UsernameAlreadyTakenException

---
+++ src/main/java/ml/echelon133/exception/UsernameAlreadyTakenException.java
@@ -0,0 +1,8 @@
+package ml.echelon133.exception;
+
+public class UsernameAlreadyTakenException extends Exception {
+
+    public UsernameAlreadyTakenException(String s) {
+        super(s);
+    }
+}
