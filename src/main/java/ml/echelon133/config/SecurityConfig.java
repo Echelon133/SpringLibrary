a09783462a67a2c4dcbab71654730ae48e377129
Mateusz Bednarski <echelon133@gmail.com>
2018-07-30 22:24:31 +0200

    Updated SecurityConfig with the config of JWT secured part of the API

--- src/main/java/ml/echelon133/config/SecurityConfig.java
+++ src/main/java/ml/echelon133/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package ml.echelon133.config;
 
+import ml.echelon133.security.jwt.JWTAuthenticationFilter;
+import ml.echelon133.security.jwt.JWTAuthenticationManager;
+import ml.echelon133.security.jwt.JWTAuthenticationProvider;
 import ml.echelon133.service.CustomUserDetailsService;
 import ml.echelon133.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +10,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
@@ -20,6 +27,63 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Order(1)
+    @Configuration
+    public static class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Bean
+        public AuthenticationProvider jwtAuthenticationProvider() {
+            return new JWTAuthenticationProvider();
+        }
+
+        @Bean
+        public AuthenticationManager jwtAuthenticationManager() {
+            return new JWTAuthenticationManager(jwtAuthenticationProvider());
+        }
+
+        @Bean
+        public RequestMatcher jwtRequestMatcher() {
+            return new AntPathRequestMatcher("/api/**");
+        }
+
+        @Bean
+        public JWTAuthenticationFilter jwtAuthenticationFilter() {
+            JWTAuthenticationFilter filter = new JWTAuthenticationFilter();
+            filter.setAuthenticationManager(jwtAuthenticationManager());
+            filter.setRequiresAuthenticationRequestMatcher(jwtRequestMatcher());
+            filter.setContinueChainBeforeSuccessfulAuthentication(true);
+            return filter;
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .csrf().disable()
+                    .anonymous().disable()
+                    .httpBasic().disable()
+                    .requestMatcher(jwtRequestMatcher())
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.GET,"/api/authors**").hasRole("USER")
+                    .antMatchers(HttpMethod.POST,"/api/authors**").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.PUT, "/api/authors/**").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.DELETE, "/api/authors/**").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.GET, "/api/genres**").hasRole("USER")
+                    .antMatchers(HttpMethod.POST, "/api/genres**").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.PUT, "/api/genres/**").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.DELETE, "/api/genres/**").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.GET, "/api/books**").hasRole("USER")
+                    .antMatchers(HttpMethod.POST, "/api/books**").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.PATCH, "/api/books/**").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.DELETE, "/api/books/**").hasRole("ADMIN")
+                    .and()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .exceptionHandling()
+                    .and()
+                    .addFilterBefore(jwtAuthenticationFilter(), BasicAuthenticationFilter.class);
+        }
+    }
+
+    @Order(2)
     @Configuration
     public static class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -34,7 +98,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         @Bean
         public RequestMatcher basicRequestMatcher() {
-            return new AntPathRequestMatcher("/api/users/**");
+            return new AntPathRequestMatcher("/users/**");
         }
 
         @Bean
@@ -54,8 +118,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .csrf().disable()
                     .requestMatcher(basicRequestMatcher())
                     .authorizeRequests()
-                    .antMatchers("/api/users/get-token").authenticated()
-                    .antMatchers("/api/users/register").anonymous()
+                    .antMatchers("/users/get-token").authenticated()
+                    .antMatchers("/users/register").anonymous()
                     .and()
                     .httpBasic()
                     .and()
