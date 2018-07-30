752033f1dfef43aa53e19552d14d3bfbe3b86121
Mateusz Bednarski <echelon133@gmail.com>
2018-07-30 18:47:23 +0200

    Added TokenController with the implementation of GET /api/users/get-token endpoint

---
+++ src/main/java/ml/echelon133/controller/TokenController.java
@@ -0,0 +1,36 @@
+package ml.echelon133.controller;
+
+import ml.echelon133.exception.FailedTokenGenerationException;
+import ml.echelon133.service.ITokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TokenController {
+
+    private ITokenService tokenService;
+
+    @Autowired
+    public TokenController(ITokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+
+    @RequestMapping(value = "api/users/get-token", method = RequestMethod.GET)
+    public ResponseEntity<Map> getToken(Principal principal) throws FailedTokenGenerationException {
+        String username = principal.getName();
+        String generatedToken = tokenService.generateTokenForUser(username);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("token", generatedToken);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
