169aa2e03cad579b483744d390e95a1d5bec0700
Mateusz Bednarski <echelon133@gmail.com>
2018-09-24 16:24:36 +0200

    Created a new package named 'book' and moved all book related classes and interfaces into it

---
+++ src/main/java/ml/echelon133/book/NewBookDto.java
@@ -0,0 +1,46 @@
+package ml.echelon133.book;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+public class NewBookDto {
+
+    @NotNull
+    @Length(min=1, max=255)
+    private String title;
+
+    @NotNull
+    @Size(min = 1, max = 10)
+    private List<Long> authorIds;
+
+    @NotNull
+    @Size(min = 1, max = 10)
+    private List<Long> genreIds;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Long> getAuthorIds() {
+        return authorIds;
+    }
+
+    public void setAuthorIds(List<Long> authorIds) {
+        this.authorIds = authorIds;
+    }
+
+    public List<Long> getGenreIds() {
+        return genreIds;
+    }
+
+    public void setGenreIds(List<Long> genreIds) {
+        this.genreIds = genreIds;
+    }
+}
